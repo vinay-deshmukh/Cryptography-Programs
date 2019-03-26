3526ff02e23d128023d1a3ee0d9af897586965b8
vinay-deshmukh <vinay_deshmukh@outlook.com>
2019-03-26 22:34:02 +0530

    move preprocess to another function

--- railfence.py
+++ railfence.py
@@ -3,28 +3,21 @@ debug_print = print if DEBUG else lambda *x: None
 DOT = '.' # denotes blank cell
 NULL = 'X'
 def print_mat(msg, mat):
-    print(msg)
+    debug_print(msg)
     for row in mat:
-        print(' '.join(row))
+        debug_print(' '.join(row))
 
-def zigzag_traversal(mat, func):
-    # mat = matrix
-    # func = function to execute for element
-    pass
+def _preprocess(inp, k):
+    # this function will make sure len(inp) is proper
+    # proper = no of elements in top row == no of elements in bottom row
 
-def enc_rail(inp, k):
-    # proper fitting inp = f(n) =  n * k + (n-1)*(k-1)
-    
-    # find n such that len(inp) is perfect 
-    # ie same no of chars in top and bottom row
-    # so add NULL to inp, if necessary
-
+    # to find correct len(inp)
     # if f(n) == len(inp), it is perfect fit, no modifications needed
     # if f(n) >  len(inp), stop iteration, add remaining chars to inp
     def f(n): return n*k + (n-1)*(k-2)
     perfect = False
     n = 0
-    for ni in range(1, 100):
+    for ni in range(1, 10000):
         an = f(ni)
         debug_print('len(inp)', len(inp), 'an', an)
         if an == len(inp):
@@ -35,9 +28,12 @@ def enc_rail(inp, k):
             break
     if not perfect:
         inp += NULL * (n - len(inp))
-    #end preprocess
 
+    return inp
 
+def enc_rail(inp, k):
+
+    inp = _preprocess(inp, k)
 
     mat = [ [ DOT for i in inp ]  for _ in range(k)]
     print_mat('Encode Init:', mat)
@@ -71,7 +67,7 @@ def dec_rail(inp, k):
         hi += 2 * (k-1)
     print_mat('First row', mat)
 
-    # second row
+    # region second row
     first_row_indices = list(range(0, len(inp), 2*(k-1)))
     second_row_indices = []
     for findex in first_row_indices:
@@ -79,13 +75,16 @@ def dec_rail(inp, k):
             second_row_indices.append( findex - 1 )
         if findex != len(inp) - 1:
             second_row_indices.append( findex + 1 )
+    
     # Remove duplicates
     second_row_indices = list(sorted(set(second_row_indices)))
+
     for si in second_row_indices:
         mat[1][si] = inp[ii]
         ii += 1
         # print_mat('First row, ii=' + str(ii), mat)
     print_mat('Second row', mat)
+    # endregion
 
     # third row onwards
     for ki in range(2, k):
@@ -112,6 +111,12 @@ def dec_rail(inp, k):
     return dec.replace(NULL, '')
 
 def _get_next_indices(mat, row_no):
+    # returns list of indices of positions to be filled in 
+    # next row, based on currently filled positions
+    # call only with row_no=1 onwards,
+    # ie call with index of second row, get indices of third row
+
+    # region Get current row indices
     non_empty_indices_cur_row = []
     row = mat[row_no]
     ri = 0
@@ -119,32 +124,36 @@ def _get_next_indices(mat, row_no):
         if row[ri] != DOT: # if non empty
             non_empty_indices_cur_row.append(ri)
         ri += 1
+    # endregion
 
     # Gen next row indices as
-    # immediately next index of first index,
-    # then for each following index, 
-    #     take index from one before and one after
+    # immediately next index for all even position index
+    # and
+    # immediately prev index for all odd  position index
+    # eg
+    # aa = [2, 5, 6]
+    # so 2 is even position, ie aa.index(2) == 0, so add next index
+    #    5 is odd  position, ie aa.index(5) == 1, so add prev index
     next_row_indices = []
-    #next_row_indices.append( non_empty_indices_cur_row[0] + 1 )
     for ni, nindex in enumerate(non_empty_indices_cur_row):
-
         if ni % 2 == 0:
-            next_row_indices.append( nindex + 1)
+            next_row_indices.append(nindex + 1)
         else:
-            next_row_indices.append( nindex - 1)
+            next_row_indices.append(nindex - 1)
 
     # Remove duplicates
     next_row_indices = sorted(set(next_row_indices))
-    debug_print('Row :', row_no)
-    debug_print('Cur :', non_empty_indices_cur_row)
-    debug_print('Next:', next_row_indices)
+
+    debug_print('CurrentRowNo   :', row_no)
+    debug_print('CurrentIndices :', non_empty_indices_cur_row)
+    debug_print('NextIndices    :', next_row_indices)
 
     return next_row_indices
     
 
 if __name__ == '__main__':
     inp = 'ABCDEFGHI' * 4
-    k = 7
+    k = 2
     assert k > 1
     enc = enc_rail(inp, k)
     dec = dec_rail(enc, k)
