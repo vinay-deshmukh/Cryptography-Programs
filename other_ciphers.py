9439c37641d95db00fcd9a3b250c34a6ede93ae7
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2019-03-29 22:16:32 +0530

    Vignere cipher added

--- other_ciphers.py
+++ other_ciphers.py
@@ -1,6 +1,6 @@
 class Ciphers:
     def __init__(self, text, key=3):
-        self.text = text
+        self.text = text.upper().replace(' ', '')
         self.key = key
 
     def _egcd(self, a, b):
@@ -20,15 +20,15 @@ class Ciphers:
             return x % m
 
     def _encode(self, msg, a=1, b=0):
-    	'''
-    	common encode function
-    	'''
-        return ''.join([ chr((( (a*(ord(t) - ord('A')) + b)) % 26) + ord('A')) for t in msg.upper().replace(' ', '') ])
+        '''
+        common encode function
+        '''
+        return ''.join([ chr((( (a*(ord(t) - ord('A')) + b)) % 26) + ord('A')) for t in msg ])
 
     def _decode(self, cipher, a=1, b=0):
-    	'''
-    	common decode function
-    	'''
+        '''
+        common decode function
+        '''
         return ''.join([ chr((( self._modinv(a, 26)*(ord(c) - ord('A') - b) ) % 26) + ord('A')) for c in cipher ])
 
     def shift_encrypt(self):
@@ -67,6 +67,24 @@ class Ciphers:
         '''
         return self._decode(cipher, a=self.key[0], b=self.key[1])
 
+    def encrypt_vignere(self):
+        '''
+        Ci = (Pi + Ki) mod 26
+        '''
+        if len(self.key) != len(self.text):
+            self.key += ''.join([ self.key[i % len(self.key)] for i in range(len(self.text) - len(self.key)) ])
+
+        elif len(self.key) > len(self.text):
+            self.key = self.text[:len(self.key)]
+
+        return ''.join([chr(((ord(a)-ord('A')) + (ord(b)-ord('A'))) % 26 + ord('A')) for a,b in zip(self.text, self.key)])
+
+    def decrypt_vignere(self, cipher):
+        '''
+        Pi = (Ci - Ki + 26) mod 26
+        '''
+        return ''.join([chr(((ord(a)-ord('A')) - (ord(b)-ord('A')) + 26) % 26 + ord('A')) for a,b in zip(cipher, self.key)])
+
 
 def main():
     text = 'twenty fifteen'
@@ -78,7 +96,7 @@ def main():
     print('Additive Cipher: ')
     print('Original Text: {}'.format(text))
     print('Encrypted Text: {}'.format(shift_encrypt_text))
-    print('Decrypted Text: {}\n'.format( ciphers.shift_decrypt(shift_encrypt_text )))
+    print('Decrypted Text: {}\n'.format( ciphers.shift_decrypt(shift_encrypt_text) ))
 
     #---------- Additive or Shift Cipher Ends ----------#
 
@@ -89,20 +107,29 @@ def main():
     print('Multiplicative Cipher: ')
     print('Original Text: {}'.format(text))
     print('Encrypted Text: {}'.format(multiplicative_encrypt_text))
-    print('Decrypted Text: {}\n'.format( ciphers.multiplicative_decrypt(multiplicative_encrypt_text )))
+    print('Decrypted Text: {}\n'.format( ciphers.multiplicative_decrypt(multiplicative_encrypt_text) ))
 
     #---------- Multiplicative Cipher Ends ----------#
 
     #------------ Affine Cipher -----------#
 
-    affine_cipher = Ciphers('twenty fifteen', [17, 20])
+    affine_cipher = Ciphers(text, [17, 20])
     affine_encrypted_text = affine_cipher.affine_encrypt()
     print('Affine Cipher: ')
     print('Original Text: {}'.format(text))
     print('Encrypted Text: {}'.format( affine_encrypted_text ))
-    print('Decrypted Text: {}'.format( affine_cipher.affine_decrypt(affine_encrypted_text) ))
+    print('Decrypted Text: {}\n'.format( affine_cipher.affine_decrypt(affine_encrypted_text) ))
 
     #--------- Affine Cipher Ends ---------#
+
+    #----------- Vignere Cipher ------------#
+    vignere = Ciphers(text, 'apex')
+    vignere_encrypted_text = vignere.encrypt_vignere()
+    print('Vignere Cipher: ')
+    print('Original Text: {}'.format(text))
+    print('Encrypted Text: {}'.format(vignere_encrypted_text))
+    print('Decrypted Text: {}'.format( vignere.decrypt_vignere(vignere_encrypted_text) ))
+    #---------- Vignere Cipher Ends --------#
 
 
 if __name__ == '__main__':
