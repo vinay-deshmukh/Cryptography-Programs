3534b94e743576cd129b2f10eb1ac19286ae6631
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2019-04-25 22:41:52 +0530

    modified bhushan's railfence_k_2.py

--- src/rail_fence/railfence_k_2_bhushan.py
+++ src/rail_fence/railfence_k_2_bhushan.py
@@ -1,20 +1,14 @@
-import math
-
 text = "ABCD"
-len_input = len(text)
+n = len(text)
 
 # encrypting
-l = [text[i::2] for i in range(2)]
-enc = ''.join(l)
+enc = ''.join(text[i::2] for i in range(2))
 print("Encrypted: ", enc)
 
 # this is the index after which the second row will start
-id1 = math.ceil(len(text)/2)
+id1 = -(-n//2)        # taking ceil of number.
 
 l1, l2 = enc[:id1], enc[id1:]
 
-for i, m in zip(l1, l2):
-    print(i + m, end='')
-
-if len_input % 2 != 0:
-	print(l1[-1])
+print("".join( sum( zip(l1, l2), () ) ))
+print(l1[-1] if n & 1 else "")
